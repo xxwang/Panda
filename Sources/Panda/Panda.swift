3d0413f4459612e38380ab57056a30e4e0b00314
xxwang <78023776@qq.com>
2023-05-20 19:23:33 +0800

    no message

--- Sources/Panda/Panda.swift
+++ Sources/Panda/Panda.swift
@@ -1,6 +1,5 @@
 public struct Panda {
     public private(set) var text = "Hello, World!"
 
-    public init() {
-    }
+    public init() {}
 }
--- Tests/PandaTests/PandaTests.swift
+++ Tests/PandaTests/PandaTests.swift
@@ -1,5 +1,5 @@
-import XCTest
 @testable import Panda
+import XCTest
 
 final class PandaTests: XCTestCase {
     func testExample() throws {
