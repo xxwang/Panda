04c11ed5e6c403932cbaa4920c193ede60c4da5b
xxwang <78023776@qq.com>
2023-05-26 14:17:21 +0800

    no message

---
+++ Sources/Panda/Extensions/HealthKit/HKActivitySummary+.swift
@@ -0,0 +1,27 @@
+//
+//  HKActivitySummary+.swift
+//  
+//
+//  Created by 王斌 on 2023/5/26.
+//
+
+import HealthKit
+
+// MARK: - 判断
+public extension HKActivitySummary {
+    
+    /// 是否完成设定的站立目标(小时数)
+    var isCompleteStandHoursGoal: Bool {
+        appleStandHoursGoal.compare(appleStandHours) != .orderedDescending
+    }
+
+    /// 检查是否达到设置的锻炼时间
+    var isCompleteExerciseTimeGoal: Bool {
+        appleExerciseTimeGoal.compare(appleExerciseTime) != .orderedDescending
+    }
+
+    /// 检查是否达到设置的活动能量
+    var isCompleteActiveEnergyBurnedGoal: Bool {
+        activeEnergyBurnedGoal.compare(activeEnergyBurned) != .orderedDescending
+    }
+}
