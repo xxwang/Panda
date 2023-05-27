8d369a455431d2d283326bc92cbbaf1659e42059
xxwang <78023776@qq.com>
2023-05-28 01:12:22 +0800

    no message

--- Sources/Panda/Extensions/UIKit/UIApplication+.swift
+++ Sources/Panda/Extensions/UIKit/UIApplication+.swift
@@ -165,3 +165,158 @@ public extension UIApplication {
         return (isSuccess: true, versions: (major: majorNumber, minor: minorNumber, patch: patchNumber))
     }
 }
+
+// MARK: - 打开操作
+public extension UIApplication {
+    /// 打开手机上的`设置App`并跳转至本App的权限界面(带提示窗)
+    /// - Parameters:
+    ///   - title:提示标题
+    ///   - message:提示内容
+    ///   - cancel:取消按钮标题
+    ///   - confirm:确认按钮标题
+    ///   - parent:来源控制器(谁来弹出提示窗)
+    static func openSettings(_ title: String?, message: String?, cancel: String = "取消", confirm: String = "设置", parent: UIViewController? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancleAction = UIAlertAction(title: cancel, style: .cancel, handler: nil)
+        alert.addAction(cancleAction)
+        let confirmAction = UIAlertAction(title: confirm, style: .default, handler: { _ in
+            // 打开系统设置App
+            self.openSettings()
+        })
+        alert.addAction(confirmAction)
+        if let root = parent {
+            root.present(alert, animated: true)
+            return
+        }
+        if let root = UIWindow.rootViewController() {
+            root.present(alert, animated: true)
+        }
+    }
+
+    /// 打开手机上的`设置App`并跳转至本App的权限界面
+    static func openSettings() {
+        guard let settingsAppURL = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+        if #available(iOS 10, *) {
+            UIApplication.shared.open(settingsAppURL, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(settingsAppURL)
+        }
+    }
+
+    /// 前往AppStore进行评价
+    /// - Parameter appID:应用在AppStore中的ID
+    static func evaluationInAppStore(_ appID: String) {
+        let appURLString = "https://itunes.apple.com/cn/app/id" + appID + "?mt=12"
+        guard let url = URL(string: appURLString), UIApplication.shared.canOpenURL(url) else {
+            return
+        }
+
+        // 打开评分页面
+        openURL(url) { isSuccess in
+            isSuccess ? print("打开应用商店评分页成功!") : print("打开应用商店评分页失败!")
+        }
+    }
+
+    /// 应用跳转
+    /// - Parameters:
+    ///   - vc:跳转时所在控制器
+    ///   - appID:app的id
+    static func updateApp(vc: UIViewController, appID: String) {
+        guard appID.count > 0 else {
+            return
+        }
+        let productView = SKStoreProductViewController()
+        // productView.delegate = (vc as! SKStoreProductViewControllerDelegate)
+        productView.loadProduct(withParameters: [SKStoreProductParameterITunesItemIdentifier: appID], completionBlock: { result, _ in
+            if !result {
+                // 点击取消
+                productView.dismiss(animated: true, completion: nil)
+            }
+        })
+        vc.showDetailViewController(productView, sender: vc)
+    }
+
+    /// 打开应用更新页面
+    /// - Parameters:
+    ///   - sourceVC:跳转时的来源控制器(需要遵守`SKStoreProductViewControllerDelegate`协议)
+    ///   - appID:用在AppStore中的ID(在Connect中创建应用后生成的ID)
+    static func updateApp(from sourceVC: some UIViewController & SKStoreProductViewControllerDelegate, appID: String) {
+        guard appID.count > 0 else {
+            return
+        }
+        let productVC = SKStoreProductViewController()
+        productVC.delegate = sourceVC
+        productVC.loadProduct(withParameters: [SKStoreProductParameterITunesItemIdentifier: appID]) { isSuccess, error in
+            productVC.dismiss(animated: true)
+            if !isSuccess {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+        }
+        sourceVC.showDetailViewController(productVC, sender: sourceVC)
+    }
+
+    /// 打开手机上安装的系统app
+    /// - Parameters:
+    ///   - app:系统App枚举
+    ///   - completion:完成回调
+    static func openSystemApp(_ app: CMSystemApp, completion: @escaping Callbacks.BoolResult) {
+        openURL(app.url, completion: completion)
+    }
+
+    /// 打开手机上安装的第三方App
+    /// - Parameters:
+    ///   - app:第三方app枚举
+    ///   - completion:完成回调
+    static func openOtherApp(_ app: CMOtherApp, completion: @escaping Callbacks.BoolResult) {
+        openURL(app.url, completion: completion)
+    }
+
+    /// 拨打电话操作
+    /// - Parameters:
+    ///   - phoneNumber:要拨打的电话号码
+    ///   - completion:完成回调
+    static func call(with phoneNumber: String, completion: @escaping Callbacks.BoolResult) {
+        // 判断是否有效
+        guard let phoneNumberEncoding = ("tel://" + phoneNumber)
+            .addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed),
+            let url = URL(string: phoneNumberEncoding),
+            UIApplication.shared.canOpenURL(url)
+        else {
+            completion(false)
+            return
+        }
+        openURL(url, completion: completion)
+    }
+
+    /// 打开指定URL地址
+    /// - Parameters:
+    ///   - url:要打开的URL地址
+    ///   - complete:完成回调
+    static func openURL(_ url: URL, completion: @escaping Callbacks.BoolResult) {
+        // iOS 10.0 以前
+        guard #available(iOS 10.0, *) else {
+            let success = UIApplication.shared.openURL(url)
+            if success {
+                print("10以前可以跳转")
+                completion(true)
+            } else {
+                print("10以前不能完成跳转")
+                completion(false)
+            }
+            return
+        }
+        // iOS 10.0 以后
+        UIApplication.shared.open(url, options: [:]) { success in
+            if success {
+                print("10以后可以跳转url")
+                completion(true)
+            } else {
+                print("10以后不能完成跳转")
+                completion(false)
+            }
+        }
+    }
+}
