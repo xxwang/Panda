9ae3a741922200410b0d36399889828705b3919b
xxwang <78023776@qq.com>
2023-05-28 17:09:37 +0800

    no message

--- Sources/Panda/Extensions/UIKit/UIImage+.swift
+++ Sources/Panda/Extensions/UIKit/UIImage+.swift
@@ -492,6 +492,30 @@ public extension UIImage {
         UIGraphicsEndImageContext()
         return newImage
     }
+
+    /// 从图片的中心点拉伸
+    /// - Returns:拉伸后的图片
+    func strechAsBubble() -> UIImage {
+        let edgeInsets = UIEdgeInsets(
+            top: size.height * 0.5,
+            left: size.width * 0.5,
+            bottom: size.height * 0.5,
+            right: size.width * 0.5
+        )
+        return resizableImage(withCapInsets: edgeInsets, resizingMode: .stretch)
+    }
+
+    /// 按`edgeInsets`与`resizingMode`拉伸图片
+    /// - Parameters:
+    ///   - edgeInsets:拉伸区域
+    ///   - resizingMode:拉伸模式
+    /// - Returns:返回拉伸后的图片
+    func strechBubble(edgeInsets: UIEdgeInsets,
+                      resizingMode: UIImage.ResizingMode = .stretch) -> UIImage
+    {
+        // 拉伸
+        resizableImage(withCapInsets: edgeInsets, resizingMode: resizingMode)
+    }
 }
 
 // MARK: - 旋转
@@ -607,7 +631,7 @@ public extension UIImage {
     /// - Parameters:
     ///   - radius:角半径(可选),如果未指定,结果图像将为圆形
     /// - Returns:带圆角的`UIImage`
-    func roundedCorners(radius: CGFloat? = nil) -> UIImage? {
+    func roundCorners(radius: CGFloat? = nil) -> UIImage? {
         let maxRadius = min(size.width, size.height) / 2
         let cornerRadius: CGFloat
         if let radius, radius > 0, radius <= maxRadius {
@@ -626,10 +650,389 @@ public extension UIImage {
         UIGraphicsEndImageContext()
         return image
     }
+
+    /// 设置图片的圆角
+    /// - Parameters:
+    ///   - size:图片的大小
+    ///   - radius:圆角大小 (默认:3.0,图片大小)
+    ///   - corners:切圆角的方式
+    /// - Returns:剪切后的图片
+    func roundCorners(size: CGSize?,
+                      radius: CGFloat,
+                      corners: UIRectCorner = .allCorners) -> UIImage?
+    {
+        let weakSize = size ?? self.size
+        let rect = CGRect(origin: CGPoint(x: 0, y: 0), size: weakSize)
+        // 开始图形上下文
+        UIGraphicsBeginImageContextWithOptions(weakSize, false, UIScreen.main.scale)
+        guard let contentRef: CGContext = UIGraphicsGetCurrentContext() else {
+            // 关闭上下文
+            UIGraphicsEndImageContext()
+            return nil
+        }
+        // 绘制路线
+        contentRef.addPath(UIBezierPath(roundedRect: rect,
+                                        byRoundingCorners: corners,
+                                        cornerRadii: CGSize(width: radius, height: radius)).cgPath)
+        // 裁剪
+        contentRef.clip()
+        // 将原图片画到图形上下文
+        draw(in: rect)
+        contentRef.drawPath(using: .fillStroke)
+        guard let output = UIGraphicsGetImageFromCurrentImageContext() else {
+            // 关闭上下文
+            UIGraphicsEndImageContext()
+            return nil
+        }
+        // 关闭上下文
+        UIGraphicsEndImageContext()
+        return output
+    }
+
+    /// 设置图片圆角(带边框)
+    /// - Parameters:
+    ///   - size:最终生成的图片尺寸
+    ///   - radius:圆角半径
+    ///   - corners:圆角方向
+    ///   - borderWidth:边框线宽
+    ///   - borderColor:边框颜色
+    ///   - backgroundColor:背景颜色
+    /// - Returns:最终图片
+    func roundCorners(size: CGSize,
+                      radius: CGFloat,
+                      corners: UIRectCorner = .allCorners,
+                      borderWidth: CGFloat = 0,
+                      borderColor: UIColor? = nil,
+                      backgroundColor: UIColor? = nil,
+                      completion: ((UIImage?) -> Void)? = nil) -> UIImage
+    {
+        // 图形上下文设置
+        UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
+        defer { UIGraphicsEndImageContext() }
+
+        // 获取当前图形上下文
+        let context = UIGraphicsGetCurrentContext()!
+
+        // 填充背景色
+        if let backgroundColor {
+            backgroundColor.setFill()
+            context.fill(CGRect(origin: .zero, size: size))
+        }
+
+        // 绘制区域的大小
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+
+        // 路径
+        var path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        if rect.size.width == rect.size.height, radius == rect.size.width / 2 {
+            path = UIBezierPath(ovalIn: rect)
+        }
+        // 添加裁剪
+        path.addClip()
+
+        // 绘制图片到上下文
+        draw(in: rect)
+
+        // 设置边框
+        if let borderColor, borderWidth > 0 {
+            path.lineWidth = borderWidth * 2
+            borderColor.setStroke()
+            path.stroke()
+        }
+
+        // 从上下文获取图片
+        let resultImage = UIGraphicsGetImageFromCurrentImageContext()
+
+        // 回调
+        DispatchQueue.async_execute_on_main {
+            completion?(resultImage)
+        }
+        return resultImage!
+    }
+
+    /// 设置圆形图片
+    /// - Returns:圆形图片
+    func roundImage() -> UIImage? {
+        roundCorners(size: size,
+                     radius: (size.width < size.height ? size.width : size.height) / 2.0,
+                     corners: .allCorners)
+    }
 }
 
 // MARK: - 颜色
 public extension UIImage {
+    /// 图像的平均颜色
+    func averageColor() -> UIColor? {
+        guard let ciImage = ciImage ?? CIImage(image: self) else { return nil }
+
+        // CIAreaAverage返回包含给定图像区域平均颜色的单像素图像
+        let parameters = [kCIInputImageKey: ciImage, kCIInputExtentKey: CIVector(cgRect: ciImage.extent)]
+        guard let outputImage = CIFilter(name: "CIAreaAverage", parameters: parameters)?.outputImage else {
+            return nil
+        }
+
+        // 从过滤器中获取单像素图像后,提取像素的RGBA8数据
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let workingColorSpace: Any = cgImage?.colorSpace ?? NSNull()
+        let context = CIContext(options: [.workingColorSpace: workingColorSpace])
+        context.render(outputImage,
+                       toBitmap: &bitmap,
+                       rowBytes: 4,
+                       bounds: CGRect(x: 0, y: 0, width: 1, height: 1),
+                       format: .RGBA8,
+                       colorSpace: nil)
+
+        // 将像素数据转换为UIColor
+        return UIColor(red: CGFloat(bitmap[0]) / 255.0,
+                       green: CGFloat(bitmap[1]) / 255.0,
+                       blue: CGFloat(bitmap[2]) / 255.0,
+                       alpha: CGFloat(bitmap[3]) / 255.0)
+    }
+
+    /// 获取图片某一个位置像素的颜色
+    /// - Parameter point:图片上某个点
+    /// - Returns:返回某个点的 UIColor
+    func pixelColor(_ point: CGPoint) -> UIColor? {
+        if point.x < 0 || point.x > size.width || point.y < 0 || point.y > size.height {
+            return nil
+        }
+
+        let provider = cgImage!.dataProvider
+        let providerData = provider!.data
+        let data = CFDataGetBytePtr(providerData)
+
+        let numberOfComponents: CGFloat = 4.0
+        let pixelData = (size.width * point.y + point.x) * numberOfComponents
+
+        let r = CGFloat(data![Int(pixelData)]) / 255.0
+        let g = CGFloat(data![Int(pixelData) + 1]) / 255.0
+        let b = CGFloat(data![Int(pixelData) + 2]) / 255.0
+        let a = CGFloat(data![Int(pixelData) + 3]) / 255.0
+
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+
+    /// 异步获取指定CGPoint位置颜色
+    func pixelColor(at point: CGPoint, completion: @escaping (UIColor?) -> Void) {
+        let size = size
+        let cgImage = cgImage
+
+        DispatchQueue.global(qos: .userInteractive).async {
+            let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            guard let imgRef = cgImage,
+                  let dataProvider = imgRef.dataProvider,
+                  let dataCopy = dataProvider.data,
+                  let data = CFDataGetBytePtr(dataCopy), rect.contains(point)
+            else {
+                DispatchQueue.main.async { completion(nil) }
+                return
+            }
+
+            let pixelInfo = (Int(size.width) * Int(point.y) + Int(point.x)) * 4
+            let red = CGFloat(data[pixelInfo]) / 255.0
+            let green = CGFloat(data[pixelInfo + 1]) / 255.0
+            let blue = CGFloat(data[pixelInfo + 2]) / 255.0
+            let alpha = CGFloat(data[pixelInfo + 3]) / 255.0
+
+            DispatchQueue.main.async {
+                completion(UIColor(red: red, green: green, blue: blue, alpha: alpha))
+            }
+        }
+    }
+
+    /// 获取图片主题颜色
+    func themeColor(_ completion: @escaping (_ color: UIColor?) -> Void) {
+        DispatchQueue.global().async {
+            if self.cgImage == nil { DispatchQueue.main.async { completion(nil) }}
+            let bitmapInfo = CGBitmapInfo(rawValue: 0).rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
+
+            // 第一步 先把图片缩小 加快计算速度. 但越小结果误差可能越大
+            let thumbSize = CGSize(width: 40, height: 40)
+            let colorSpace = CGColorSpaceCreateDeviceRGB()
+            guard let context = CGContext(data: nil,
+                                          width: Int(thumbSize.width),
+                                          height: Int(thumbSize.height),
+                                          bitsPerComponent: 8,
+                                          bytesPerRow: Int(thumbSize.width) * 4,
+                                          space: colorSpace,
+                                          bitmapInfo: bitmapInfo) else { return completion(nil) }
+
+            let drawRect = CGRect(x: 0, y: 0, width: thumbSize.width, height: thumbSize.height)
+            context.draw(self.cgImage!, in: drawRect)
+
+            // 第二步 取每个点的像素值
+            if context.data == nil { return completion(nil) }
+            let countedSet = NSCountedSet(capacity: Int(thumbSize.width * thumbSize.height))
+            for x in 0 ..< Int(thumbSize.width) {
+                for y in 0 ..< Int(thumbSize.height) {
+                    let offset = 4 * x * y
+                    let red = context.data!.load(fromByteOffset: offset, as: UInt8.self)
+                    let green = context.data!.load(fromByteOffset: offset + 1, as: UInt8.self)
+                    let blue = context.data!.load(fromByteOffset: offset + 2, as: UInt8.self)
+                    let alpha = context.data!.load(fromByteOffset: offset + 3, as: UInt8.self)
+                    // 过滤透明的、基本白色、基本黑色
+                    if alpha > 0, red < 250, green < 250, blue < 250, red > 5, green > 5, blue > 5 {
+                        let array = [red, green, blue, alpha]
+                        countedSet.add(array)
+                    }
+                }
+            }
+
+            // 第三步 找到出现次数最多的那个颜色
+            let enumerator = countedSet.objectEnumerator()
+            var maxColor: [Int] = []
+            var maxCount = 0
+            while let curColor = enumerator.nextObject() as? [Int], !curColor.isEmpty {
+                let tmpCount = countedSet.count(for: curColor)
+                if tmpCount < maxCount { continue }
+                maxCount = tmpCount
+                maxColor = curColor
+            }
+            let color = UIColor(red: CGFloat(maxColor[0]) / 255.0, green: CGFloat(maxColor[1]) / 255.0, blue: CGFloat(maxColor[2]) / 255.0, alpha: CGFloat(maxColor[3]) / 255.0)
+            DispatchQueue.main.async { completion(color) }
+        }
+    }
+
+    /// 获取图片背景/主要/次要/细节 颜色
+    /// - Parameter scaleDownSize:指定图片大小
+    /// - Returns:背景/主要/次要/细节 颜色
+    func colors(scaleDownSize: CGSize? = nil) -> (background: UIColor, primary: UIColor, secondary: UIColor, detail: UIColor) {
+        let cgImage: CGImage
+
+        if let scaleDownSize {
+            cgImage = resize(to: scaleDownSize).cgImage!
+        } else {
+            let ratio = size.width / size.height
+            let r_width: CGFloat = 250
+            cgImage = resize(to: CGSize(width: r_width, height: r_width / ratio)).cgImage!
+        }
+
+        let width = cgImage.width
+        let height = cgImage.height
+        let bytesPerPixel = 4
+        let bytesPerRow = width * bytesPerPixel
+        let bitsPerComponent = 8
+        let randomColorsThreshold = Int(CGFloat(height) * 0.01)
+        let blackColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        let whiteColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let raw = malloc(bytesPerRow * height)
+        let bitmapInfo = CGImageAlphaInfo.premultipliedFirst.rawValue
+        let context = CGContext(data: raw, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+        context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height)))
+        let data = UnsafePointer<UInt8>(context?.data?.assumingMemoryBound(to: UInt8.self))
+        let imageBackgroundColors = NSCountedSet(capacity: height)
+        let imageColors = NSCountedSet(capacity: width * height)
+
+        let sortComparator: (CountedColor, CountedColor) -> Bool = { a, b -> Bool in
+            a.count <= b.count
+        }
+
+        for x in 0 ..< width {
+            for y in 0 ..< height {
+                let pixel = ((width * y) + x) * bytesPerPixel
+                let color = UIColor(
+                    /*
+                     red:CGFloat(data?[pixel + 1]!) / 255,
+                     green:CGFloat(data?[pixel + 2]!) / 255,
+                     blue:CGFloat(data?[pixel + 3]!) / 255,
+                     */
+                    red: CGFloat(data?[pixel + 1] ?? 0) / 255,
+                    green: CGFloat(data?[pixel + 2] ?? 0) / 255,
+                    blue: CGFloat(data?[pixel + 3] ?? 0) / 255,
+                    alpha: 1
+                )
+
+                if x >= 5, x <= 10 {
+                    imageBackgroundColors.add(color)
+                }
+
+                imageColors.add(color)
+            }
+        }
+
+        var sortedColors = [CountedColor]()
+
+        for color in imageBackgroundColors {
+            guard let color = color as? UIColor else { continue }
+
+            let colorCount = imageBackgroundColors.count(for: color)
+
+            if randomColorsThreshold <= colorCount {
+                sortedColors.append(CountedColor(color: color, count: colorCount))
+            }
+        }
+
+        sortedColors.sort(by: sortComparator)
+
+        var proposedEdgeColor = CountedColor(color: blackColor, count: 1)
+
+        if let first = sortedColors.first { proposedEdgeColor = first }
+
+        if proposedEdgeColor.color.isBlackOrWhite, !sortedColors.isEmpty {
+            for countedColor in sortedColors where CGFloat(countedColor.count / proposedEdgeColor.count) > 0.3 {
+                if !countedColor.color.isBlackOrWhite {
+                    proposedEdgeColor = countedColor
+                    break
+                }
+            }
+        }
+
+        let imageBackgroundColor = proposedEdgeColor.color
+        let isDarkBackgound = imageBackgroundColor.isDark
+
+        sortedColors.removeAll()
+
+        for imageColor in imageColors {
+            guard let imageColor = imageColor as? UIColor else { continue }
+
+            let color = imageColor.color(minSaturation: 0.15)
+
+            if color.isDark == !isDarkBackgound {
+                let colorCount = imageColors.count(for: color)
+                sortedColors.append(CountedColor(color: color, count: colorCount))
+            }
+        }
+
+        sortedColors.sort(by: sortComparator)
+
+        var primaryColor, secondaryColor, detailColor: UIColor?
+
+        for countedColor in sortedColors {
+            let color = countedColor.color
+
+            if primaryColor == nil,
+               color.isContrasting(with: imageBackgroundColor)
+            {
+                primaryColor = color
+            } else if secondaryColor == nil,
+                      primaryColor != nil,
+                      primaryColor!.isDistinct(from: color),
+                      color.isContrasting(with: imageBackgroundColor)
+            {
+                secondaryColor = color
+            } else if secondaryColor != nil,
+                      secondaryColor!.isDistinct(from: color),
+                      primaryColor!.isDistinct(from: color),
+                      color.isContrasting(with: imageBackgroundColor)
+            {
+                detailColor = color
+                break
+            }
+        }
+
+        free(raw)
+
+        return (
+            imageBackgroundColor,
+            primaryColor ?? (isDarkBackgound ? whiteColor : blackColor),
+            secondaryColor ?? (isDarkBackgound ? whiteColor : blackColor),
+            detailColor ?? (isDarkBackgound ? whiteColor : blackColor)
+        )
+    }
+
     /// `UIImage`填充颜色
     ///
     /// - Parameter color:填充图像的颜色
@@ -688,9 +1091,7 @@ public extension UIImage {
         #endif
 
         UIGraphicsBeginImageContextWithOptions(size, false, scale)
-        defer {
-            UIGraphicsEndImageContext()
-        }
+        defer { UIGraphicsEndImageContext() }
         let context = UIGraphicsGetCurrentContext()
         color.setFill()
         context?.fill(drawRect)
@@ -728,32 +1129,4 @@ public extension UIImage {
 }
 
 // MARK: - 方法
-public extension UIImage {
-    /// 图像的平均颜色
-    func averageColor() -> UIColor? {
-        guard let ciImage = ciImage ?? CIImage(image: self) else { return nil }
-
-        // CIAreaAverage返回包含给定图像区域平均颜色的单像素图像
-        let parameters = [kCIInputImageKey: ciImage, kCIInputExtentKey: CIVector(cgRect: ciImage.extent)]
-        guard let outputImage = CIFilter(name: "CIAreaAverage", parameters: parameters)?.outputImage else {
-            return nil
-        }
-
-        // 从过滤器中获取单像素图像后,提取像素的RGBA8数据
-        var bitmap = [UInt8](repeating: 0, count: 4)
-        let workingColorSpace: Any = cgImage?.colorSpace ?? NSNull()
-        let context = CIContext(options: [.workingColorSpace: workingColorSpace])
-        context.render(outputImage,
-                       toBitmap: &bitmap,
-                       rowBytes: 4,
-                       bounds: CGRect(x: 0, y: 0, width: 1, height: 1),
-                       format: .RGBA8,
-                       colorSpace: nil)
-
-        // 将像素数据转换为UIColor
-        return UIColor(red: CGFloat(bitmap[0]) / 255.0,
-                       green: CGFloat(bitmap[1]) / 255.0,
-                       blue: CGFloat(bitmap[2]) / 255.0,
-                       alpha: CGFloat(bitmap[3]) / 255.0)
-    }
-}
+public extension UIImage {}
