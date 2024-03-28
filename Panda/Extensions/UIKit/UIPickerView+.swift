c162235faff413e6d1dff7115f75c7765c4eaff5
xxwang <78023776@qq.com>
2024-03-28 17:31:19 +0800

    no message

---
+++ Panda/Extensions/UIKit/UIPickerView+.swift
@@ -0,0 +1,34 @@
+//
+//  UIPickerView+.swift
+//  BaseFramework
+//
+//  Created by apple on 2024/3/28.
+//
+
+import UIKit
+
+
+public extension UIPickerView {
+    typealias Associatedtype = UIPickerView
+
+    override class func `default`() -> Associatedtype {
+        let pickerView = UIPickerView()
+        return pickerView
+    }
+}
+
+
+extension UIPickerView {
+    
+    @discardableResult
+    func pd_delegate(_ delegate: UIPickerViewDelegate) -> Self {
+        self.delegate = delegate
+        return self
+    }
+
+    @discardableResult
+    func pd_dataSource(_ dataSource: UIPickerViewDataSource) -> Self {
+        self.dataSource = dataSource
+        return self
+    }
+}
