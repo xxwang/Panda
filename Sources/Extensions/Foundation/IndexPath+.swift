0011f2d87dfaf03e7bdd4a3ae09becd42ccba4fd
wange <78023776@qq.com>
2024-06-10 23:26:12 +0800

    no message

--- Sources/Extensions/Foundation/IndexPath+.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-public extension IndexPath {
-    /// 用字符串描述`IndexPath`
-    /// - Returns: 字符串描述
-    func pd_string() -> String {
-        return String(format: "{section: %d, row: %d}", section, row)
-    }
-}
---
+++ Sources/PandaKit.swift
@@ -0,0 +1,8 @@
+//
+//  PandaKit.swift
+//
+//
+//  Created by wange on 2024/6/10.
+//
+
+import Foundation
